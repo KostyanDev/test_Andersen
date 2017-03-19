ff9e7721fef16f5f60ef2dfe04bd0220f6cae672
Константин Овчаренко <HardBuf@MBPKonstantin2.Dlink>
2017-03-19 11:58:47 +0200

    Add VIew

---
+++ test_Andersen/test_Andersen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_Andersen
+//
+//  Created by Константин Овчаренко on 19.03.17.
+//  Copyright © 2017 Константин Овчаренко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
